6e0ccfb531d2e056dca189f9d2e958e0ca3e1739
Caique de Paula Figueiredo Coelho <caique.coelho@raidiam.com>
2025-10-17 12:02:39 -0300

    remove comments

--- test/validators.test.js
+++ test/validators.test.js
@@ -424,12 +424,9 @@ describe('Validators', () => {
         'http://[2010:836B:4179::836B:4179]',
         'http://example.com/example.json#/foo/bar',
         'http://1337.com',
-        // TODO: those should not be marked as valid URLs; CVE-2025-56200
-        /* eslint-disable no-script-url */
         'javascript:%61%6c%65%72%74%28%31%29@example.com',
         'http://evil-site.com@example.com/',
         'ｊａｖａｓｃｒｉｐｔ:alert(1)@example.com',
-        /* eslint-enable no-script-url */
       ],
       invalid: [
         'http://localhost:3000/',
@@ -472,8 +469,6 @@ describe('Validators', () => {
         '////foobar.com',
         'http:////foobar.com',
         'https://example.com/foo/<script>alert(\'XSS\')</script>/',
-        // the following tests are because of CVE-2025-56200
-        /* eslint-disable no-script-url */
         "javascript:alert(1);a=';@example.com/alert(1)'",
         'JaVaScRiPt:alert(1)@example.com',
         'javascript:/* comment */alert(1)@example.com',
@@ -483,7 +478,6 @@ describe('Validators', () => {
         'data:text/html,<script>alert(1)</script>@example.com',
         'vbscript:msgbox("XSS")@example.com',
         '//evil-site.com/path@example.com',
-        /* eslint-enable no-script-url */
       ],
     });
   });
@@ -827,7 +821,6 @@ describe('Validators', () => {
       }],
       valid: [],
       invalid: [
-        // eslint-disable-next-line no-script-url
         "javascript:alert(1);a=';@example.com/alert(1)",
       ],
     });
@@ -893,9 +886,7 @@ describe('Validators', () => {
         'https://user:pass@example.com',
       ],
       invalid: [
-        // eslint-disable-next-line no-script-url
         'javascript:alert(1);@example.com',
-        // eslint-disable-next-line no-script-url
         'javascript:void(0);@example.com',
         'data:text/html,<script>alert(1)</script>@example.com',
         'file:something;invalid@host.com',
